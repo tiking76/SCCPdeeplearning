b56ed78a3afb46debd417e0c6a423b9a93f4b108
tiking76 <716yosiki1999@gmail.com>
2020-01-15 22:52:22 +0900

    prototype

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
-from keras import backend as K
-from keras.datasets import mnist
-import matplotlib.pyplot as plt
+"""This is a test program."""
+import cv2
+import numpy as np
+from keras.models import load_model
 
 
-# mnist データをダウンロード
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-# 画像データとラベルの要素数を表示
-print("画像データの要素数", train_images.shape)
-print("ラベルデータの要素数", train_labels.shape)
 
-# ラベルと画像データを表示
-for i in range(0, 10):
- print("ラベル", train_labels[i])
- plt.imshow(train_images[i].reshape(28, 28), cmap='Greys')
- plt.show()
+# ここの数字は0はwebカメラ使用時、それ以外は1オリジンでやっていく。失敗したときは-1が帰ってくる
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+
+    h, w, _ = frame.shape[:3]
+
+    w_center = w//2
+    h_center = h//2
+
+    cv2.rectangle(frame, (int(w_center - 71), int(h_center - 71)),
+                  (int(w_center + 71), int(h_center + 71)), (255, 0, 0))
+    cv2.imshow("frame", frame)
+
+    k = cv2.waitKey(1) & 0xFF
+    prop_val = cv2.getWindowProperty("frame", cv2.WND_PROP_ASPECT_RATIO)
+
+    if k == ord("q") or (prop_val < 0):
+        break
+    elif k == ord("s"):
+        im = frame[h_center - 70:h_center + 70, w_center - 70:w_center + 70]
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        _, th = cv2.threshold(im, 0, 255, cv2.THRESH_OTSU)
+        th = cv2.bitwise_not(th)
+        th = cv2.GaussianBlur(th, (9, 9), 0)
+        cv2.imwrite("capture.jpg", th)
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+Xt = []
+img = cv2.imread("capture.jpg", 0)
+img = cv2.resize(img, (28, 28), cv2.INTER_CUBIC)
+
+Xt.append(img)
+Xt = np.array(Xt)/255
+
+model = load_model("MNIST.h5")
+
+result = model.predict_classes(Xt)
+
+print("結果", result[0])
